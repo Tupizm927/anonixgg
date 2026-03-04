edc905dc7fff2454fba077b1ebf6dcf4d45ac88a
Tupizm927 <156996897+Tupizm927@users.noreply.github.com>
2026-03-04 12:18:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -348,35 +348,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
             await send_notification_to_chat(context, notification_text)
             return
-    keyboard = [
-    # 1 ряд
-    [
-        InlineKeyboardButton(get_text(lang, "create_deal_button"), callback_data='create_deal'),
-        InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu'),
-    ],
-
-    # 2 ряд
-    [
-        InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral'),
-        InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info'),
-    ],
-
-    # 3 ряд
-    [
-        InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang'),
-    ],
-
-    # 4 ряд
-    [
-        InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups'),
-        InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam'),
-    ],
-
-    # 5 ряд
-    [
-        InlineKeyboardButton(get_text(lang, "webapp_button"), web_app=WebAppInfo(url="https://lzt.market")),
-    ],
-]
+        keyboard = [
+            [InlineKeyboardButton(get_text(lang, "create_deal_button"), callback_data='create_deal')],
+            [InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu')],
+            [InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral')],
+            [InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info')],
+            [InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang')],
+            [InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups')],
+            [InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam')],
+            [InlineKeyboardButton(get_text(lang, "webapp_button"), web_app=WebAppInfo(url="https://lzt.market"))],
+        ]
         if user_id in ADMIN_ID:
             keyboard.append([InlineKeyboardButton("🔧 Воркер Меню", callback_data='admin_panel')])
 
@@ -1237,6 +1218,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
