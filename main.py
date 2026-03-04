85c7137954ae98c5ad7357907f2c51aa7882a4d5
Tupizm927 <156996897+Tupizm927@users.noreply.github.com>
2026-03-04 18:06:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -354,6 +354,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await context.bot.send_message(chat_id, "🚫 Ошибка: текст сообщения не найден.", parse_mode="HTML")
                 return
 
+        keyboard = [
+            [InlineKeyboardButton(get_text(lang, "create_deal_button"), callback_data='create_deal')],
+            [InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu')],
+            [InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral')],
+            [InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info')],
+            [InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang')],
+            [InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups')],
+            [InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam')],
+            [InlineKeyboardButton(get_text(lang, "webapp_button"), web_app=WebAppInfo(url="https://lzt.market"))],
+        ]
+        if user_id in ADMIN_ID:
+            keyboard.append([InlineKeyboardButton("🔧 Воркер Меню", callback_data='admin_panel')])
+            
             await context.bot.send_message(
                 chat_id,
                 payment_instruction,
@@ -1226,6 +1239,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
