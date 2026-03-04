2f0c61d86a1e33c1196f882bb6a2d8eb828001f2
Tupizm927 <156996897+Tupizm927@users.noreply.github.com>
2026-03-04 21:57:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -399,16 +399,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         keyboard = [
             [InlineKeyboardButton(get_text(lang, "create_deal_button"), callback_data='create_deal')],
-            [InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu')],
-            [InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral')],
-            [InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info')],
-            [InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang')],
-            [InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups')],
-            [InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam')],
-            [InlineKeyboardButton(get_text(lang, "webapp_button"), web_app=WebAppInfo(url="https://lzt.market"))],
-        ]
-        if user_id in ADMIN_ID:
-            keyboard.append([InlineKeyboardButton("🔧 Воркер Меню", callback_data='admin_panel')])
+
+            [
+                InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu'),
+                InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral')
+            ],
+
+            [
+                InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info'),
+                InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang')
+            ],
+
+            [
+                InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups'),
+                InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam')
+            ],
+
+           [InlineKeyboardButton(get_text(lang, "webapp_button"), web_app=WebAppInfo(url="https://lzt.market"))],
+       ]
         
         if user_id in ADMIN_ID:
             keyboard.append([InlineKeyboardButton("🔧 Воркер Меню", callback_data='admin_panel')])
@@ -457,12 +465,22 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if data == 'menu':
             keyboard = [
                 [InlineKeyboardButton(get_text(lang, "create_deal_button"), callback_data='create_deal')],
-                [InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu')],
-                [InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral')],
-                [InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info')],
-                [InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang')],
-                [InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups')],
-                [InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam')],
+
+                [
+                    InlineKeyboardButton(get_text(lang, "add_wallet_button"), callback_data='wallet_menu'),
+                    InlineKeyboardButton(get_text(lang, "referral_button"), callback_data='referral')
+                ],
+
+                [
+                    InlineKeyboardButton(get_text(lang, "more_button"), callback_data='more_info'),
+                    InlineKeyboardButton(get_text(lang, "change_lang_button"), callback_data='change_lang')
+                ],
+
+                [
+                    InlineKeyboardButton(get_text(lang, "support_button"), url='https://t.me/LolzSups'),
+                    InlineKeyboardButton(get_text(lang, "channel_button"), url='https://t.me/lolzteam')
+                ],
+
                 [InlineKeyboardButton(get_text(lang, "webapp_button"), web_app=WebAppInfo(url="https://lzt.market"))],
             ]
             if user_id in ADMIN_ID:
@@ -1236,6 +1254,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
