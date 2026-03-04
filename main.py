f8014fb2e1f76b8d8dea602202cd4fe6f412f579
Tupizm927 <156996897+Tupizm927@users.noreply.github.com>
2026-03-04 13:17:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -778,6 +778,16 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
             admin_commands[user_id] = 'broadcast'
 
+        elif data == 'more_info':
+            message_text = get_text(lang, "more_info")
+            await query.edit_message_caption(
+                caption=message_text,
+                parse_mode="HTML",
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton(get_text(lang, "menu_button"), callback_data='menu')]
+                ])
+            )
+        
         elif data == 'admin_list' and user_id in ADMIN_ID:
             admin_list_entries = []
             for admin_id_loop in ADMIN_ID:
@@ -1222,6 +1232,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
